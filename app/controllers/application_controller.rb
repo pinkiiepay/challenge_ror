c5465aab6c4ec6048961c4b36a3fab6879591d3d
Jose Aguirre <ora.tucan@gmail.com>
2024-05-14 00:08:17 -0600

    Se agregaron las temperaturas por geoloc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::API
         if params[:city] != ""
             city = params[:city]
             response = RestClient.get 'https://search.reservamos.mx/api/v2/places', {params: {q: city}}
-            puts response.code
+            # puts response.code
             if response.code == 201
                 json_object = JSON.parse(response)
                 json_string = "["
                 for ciudad in json_object
                     if ciudad["result_type"] == "city"
-                        json_string+= '{"city_name":"' + ciudad["city_name"] +'","latitud":"' + ciudad["lat"] +'","longitud":"' + ciudad["long"] +'"},'
+                        json_string+= '{"ciudad":"' + ciudad["city_name"] +'","latitud":"' + ciudad["lat"] +'","estado":"' + ciudad["state"] +'","longitud":"' + ciudad["long"] +'"},'
                     end
                 end
                 if json_string != "["
@@ -21,11 +21,33 @@ class ApplicationController < ActionController::API
                 end
                 # puts json_string.class
                 json_cities = JSON.parse(json_string)
+
                 #Parte de búsqueda de clima por ciudad para los 7 días de la semana
-                # for ciudad in json_cities
-                    
-                #     response_wheather = RestClient.get 'https://api.openweathermap.org/data/2.5/onecall', {params: {lat: ciudad["latitud"], lon: ciudad["longitud"], exclude: "current,minutely,hourly,alerts", lang:"sp", units:"metric", appid:"a5a47c18197737e8eeca634cd6acb581" }}
-                # end
+                for ciudad in json_cities          
+                    response_wheather = RestClient.get 'https://api.openweathermap.org/data/2.5/forecast', {params: {lat:ciudad["latitud"] , lon:ciudad["longitud"], lang:"sp", units:"metric", appid:"fce130528c880e622fefde5bad9587e5" }}
+                    json_weather = JSON.parse(response_wheather)
+                    puts "holaaaa"
+                    weather_array = []
+                    n=0
+                    for weather_list in json_weather["list"]
+                        if weather_list["dt_txt"].include? "12:00:00"
+                            # puts weather_list["main"]["temp_min"]
+                            temp_min = weather_list["main"]["temp_min"].to_s
+                            temp_max = weather_list["main"]["temp_max"].to_s
+                            day = weather_list["dt_txt"].slice!(0..9)
+                            weather_string = '{"dia":"'+ day +'","temperatura_minima":"' + temp_min +'°","temperatura_maxima":"' + temp_max +'°"}'
+                            # puts weather_string
+                            #weather_string = '{"t":"12°","t2":"45°"}'
+                            json_weather_city = JSON.parse(weather_string)
+                            # puts json_weather_city
+                            weather_array[n] = json_weather_city
+                            n = n+1
+                        end
+                    end
+                    ciudad["temperatura"] = weather_array
+                    #puts ciudad
+                end
+
                 render json: { ciudades: json_cities}
             else
                 render json: {Error: "No hay ciudades"}
